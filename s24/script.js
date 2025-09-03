07d48ffd8e7d5253560ee34a7405c3707a69bf17
Dacsc <daicruzsantacruz@crackthecode.la>
2025-09-03 10:24:47 -0500

    agregue la sesion 24

---
+++ s24/script.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo");
+
+let valor=true;
+
+console.log(valor);
+console.log(typeof(valor));
+
+let nombre=3.44;
+console.log(nombre);
+console.log(typeof(nombre));
+
+let numero=2
+console.log(numero);
+console.log(typeof(numero));
+
+let palabra="buen \ndia";
+console.log(palabra);
+console.log(typeof(palabra));
+
+
+
